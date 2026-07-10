c1fa7781002516798eea89a438219087ac507073
ByMadeeha <madeeha.od@outlook.com>
2026-07-10 23:12:37 +0600

    Import AuthProvider and UserProvider components

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { UserProvider } from "./context/UserContext";
+
 
 import App from "./App";
 
